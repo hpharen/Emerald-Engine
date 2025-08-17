30ff2540b0d73c3b315cd2bd2b0e295ff25c034a
Haren Patel <hp.haren@gmail.com>
2025-08-17 18:10:31 -0400

    automatically opens bizhawk now if not open

--- lucy.py
+++ lucy.py
@@ -1,8 +1,44 @@
-import random
-import time
 import os
-
+import time
+import random
+import psutil
+import win32gui
+import win32process
+bizhawk_path = r"C:\Users\Haren\Downloads\BizHawk-2.10-win-x64\EmuHawk.exe"
 ACTIONS = ["A","B","Up","Down","Left","Right"]
+
+def open_bizhawk():
+    print("Opening Bizhawk...")
+    os.startfile(bizhawk_path)
+
+def bizhawk_window(pids):
+    titles = []
+    returnpid = 0
+    def _enum_cb(hwnd, results):
+        if win32gui.IsWindowVisible(hwnd):
+            pid = win32process.GetWindowThreadProcessId(hwnd)[1] 
+            if pids is None or pid in pids:
+                nonlocal returnpid
+                returnpid = pid
+    win32gui.EnumWindows(_enum_cb, titles)
+    return returnpid
+
+program_name = "EmuHawk.exe"
+timeout = time.time() + 120
+isOpen = False
+
+# Check if Bizhawk is open, otherwise open it
+for process in psutil.process_iter():
+    try:
+        if process.name() == program_name:
+            print("Bizhawk is open.")
+            isOpen = True
+            break
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+        pass
+else:
+    print("Bizhawk is not open.")
+    open_bizhawk()
 
 while True:
     # Wait until state.txt exists
